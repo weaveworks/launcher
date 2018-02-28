c0a9044346a4bc35dfc43dbf68e396c03b31afa9
Damien Lespiau <damien@weave.works>
2018-02-28 18:57:06 +0000

    sentry: Move the skipFrames argument first and tweak its semantics
    - Now we can wrap Capture() with helpers that take variadic arguments and still
      preserve the order of the format string vs skipFrames
    
    - Changed the meaning of skipFrames to exclude Capture() iself. To catter for
      the previous check, make sure we can't have negative values by using a uint.

--- pkg/sentry/sentry.go
+++ pkg/sentry/sentry.go
@@ -4,24 +4,25 @@ import (
 	raven "github.com/getsentry/raven-go"
 )
 
-// Capture reports an error message to sentry along with a stacktrace.
+// CaptureAndWait reports an error message to sentry along with a stacktrace.
 //
 // skipFrames can be used to indicate how many stack frames should be skipped to
-// reach the frame where the error actually happened instead of always pointing
-// to a generic "logError()".
+// reach the frame where the error actually happened, eg.:
+// - When Capture is called directly from the location that should appear in the
+// stack trace, skipFrames should be 0.
+// - When Capture is called from a helper function, eg. to both log to stdout
+// and sentry, that helper function frame can be skipped by giving 1.
 //
-// tags are additional tags to add the sentry event.
+// tags are additional tags to add to the sentry event.
 //
 // This call is synchronous in that it will not return until the message has
 // been sent to sentry.
-func Capture(message string, skipFrames int, tags map[string]string) {
-	// We want to at least skip the Capture() function itself.
-	if skipFrames < 1 {
-		skipFrames = 1
-	}
+func CaptureAndWait(skipFrames uint, message string, tags map[string]string) {
+	// We want to skip the Capture() function itself.
+	skipFrames++
 
 	// Capture the error with stacktrace and wait.
-	stack := raven.NewStacktrace(skipFrames, 3, nil)
+	stack := raven.NewStacktrace(int(skipFrames), 3, nil)
 	packet := raven.NewPacket(message, stack)
 	eventID, ch := raven.Capture(packet, tags)
 	if eventID != "" {
