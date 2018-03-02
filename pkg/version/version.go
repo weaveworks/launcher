e58ee4128c86c6af00d08d02d048a47161fc38b2
Marcus Cobden <marcus@weave.works>
2018-03-02 12:08:20 +0000

    Build version info into binaries

---
+++ pkg/version/version.go
@@ -0,0 +1,7 @@
+package version
+
+// Version is the current version number, e.g: v0.0-251-ge29921f
+var Version string
+
+// GitRevision is the git revision, e.g: e29921fb682f610b1663efaee278e8d5551b9051
+var GitRevision string
