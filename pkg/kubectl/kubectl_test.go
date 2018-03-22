da8c80e979e6a8be1a13ca8d2bbf6e67d4e8b8de
Marcus Cobden <marcus@weave.works>
2018-03-22 14:42:02 +0000

    Use full --output flag for kubectl 1.7 compatability

--- pkg/kubectl/kubectl_test.go
+++ pkg/kubectl/kubectl_test.go
@@ -35,7 +35,7 @@ func TestGetSecretValue(t *testing.T) {
 	tc := NewTestClient()
 
 	json := `{"data":{"token": "c2VjcmV0IQ=="}}`
-	tc.responses["get secret weave-cloud --namespace=weave -ojson"] = json
+	tc.responses["get secret weave-cloud --namespace=weave --output=json"] = json
 	res, err := GetSecretValue(tc, "weave", "weave-cloud", "token")
 	assert.Equal(t, res, "secret!")
 	assert.NoError(t, err)
