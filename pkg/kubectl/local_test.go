cde1a10d17ee3df2ef21305e06fc5ecda12e73bc
Damien Lespiau <damien@weave.works>
2018-04-05 17:21:56 +0100

    kubectl: Fix unit tests that aren't run in CI (!)
    The function tested has changed a little.

--- pkg/kubectl/local_test.go
+++ pkg/kubectl/local_test.go
@@ -7,18 +7,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFormatCmdOutput(t *testing.T) {
+func TestTrimOutput(t *testing.T) {
 	tests := []struct {
-		input    []byte
+		input    string
 		expected string
 	}{
-		{[]byte("foo"), "foo"},
-		{[]byte("'foo'"), "foo"},
-		{[]byte("  'foo'  "), "foo"},
+		{"foo", "foo"},
+		{"'foo'", "foo"},
+		{"  'foo'  ", "foo"},
 	}
 
 	for _, tc := range tests {
-		if output := formatCmdOutput(tc.input); output != tc.expected {
+		if output := trimOutput(tc.input); output != tc.expected {
 			t.Errorf("Expected %s, got: %s", tc.expected, output)
 		}
 	}
